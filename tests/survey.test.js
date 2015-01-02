7d4932f44f410c37a649d0b0de35bb223bb2e2d5
jjesus <john.j.jesus@gmail.com>
2015-01-02 13:38:20 -0600

    Add answerList to Survey.

--- tests/survey.test.js
+++ tests/survey.test.js
@@ -6,9 +6,12 @@ describe("survey", function() {
     afterEach(function() {
     });
 
-    it("survey:score exists", function() {
+    it("score exists", function() {
         console.log(Survey);
         expect(Survey.score).toBeDefined();
+    });
+    it("q1 answer", function() {
+        Survey.answer('q1', {value:21});
         Survey.score();
         expect(Survey.score).toHaveBeenCalled();
     });
