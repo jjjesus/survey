27c12300b73687a63097f6a60a8b1ac0d0b3a179
jjesus <john.j.jesus@gmail.com>
2015-01-02 14:44:48 -0600

    Fix bc names. Score just prior to Results.

--- tests/survey.test.js
+++ tests/survey.test.js
@@ -7,15 +7,14 @@ describe("survey", function() {
     });
 
     it("score exists", function() {
-        console.log(Survey);
         expect(Survey.score).toBeDefined();
     });
     it("q1 answer", function() {
         Survey.answer('q1', {value:16});
+    });
+    it("score and results", function() {
         Survey.score();
         expect(Survey.score).toHaveBeenCalled();
-    });
-    it("results", function() {
         Survey.results();
     });
 });
